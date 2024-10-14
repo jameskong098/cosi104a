1a2100f89dd8da039c9da7e49215210cdb7f06f4
jameskong <jameskong098@gmail.com>
2024-10-14 01:39:08 -0400

    remove redudant cross validation

--- HW4/model.py
+++ HW4/model.py
@@ -34,10 +34,26 @@ def train_and_evaluate_model(X_train, y_train, feature_names):
     print(f"Features: {feature_names}")
 
     param_file = 'hyper_parameters.txt'
+    highest_f1_file = 'highest_f1_score.txt'
+
     if os.path.exists(param_file):
         with open(param_file, 'r') as file:
             best_params = json.load(file)
         print(f"\nLoaded best parameters from {param_file}: {best_params}")
+
+        model = DecisionTreeClassifier(random_state=42, **best_params)
+        f1_scorer = make_scorer(f1_score)
+        scores = cross_val_score(model, X_train, y_train, cv=5, scoring=f1_scorer)
+        mean_f1_score = scores.mean()
+        best_score = mean_f1_score
+        print(f'\nCross-Validation F1 Scores: {scores}')
+        print(f'Mean F1 Score: {mean_f1_score}\n')
+
+        if os.path.exists(highest_f1_file):
+            with open(highest_f1_file, 'r') as file:
+                highest_f1_score = float(file.read())
+        else:
+            highest_f1_score = 0.0
     else:
         param_grid = {
             'max_depth': [3, 5, 7, 10, 15, 20, 25],
@@ -54,29 +70,23 @@ def train_and_evaluate_model(X_train, y_train, feature_names):
             json.dump(best_params, file)
         print(f"\nSaved best parameters to {param_file}: {best_params}")
 
-    model = DecisionTreeClassifier(random_state=42, **best_params)
-    f1_scorer = make_scorer(f1_score)
-    scores = cross_val_score(model, X_train, y_train, cv=5, scoring=f1_scorer)
-
-    print(f'\nCross-Validation F1 Scores: {scores}')
-    mean_f1_score = scores.mean()
-    print(f'Mean F1 Score: {mean_f1_score}\n')
+        best_score = grid_search.best_score_
+        print(f"\nHighest F1 Score from GridSearchCV: {best_score}")
 
     # Save the highest validation F1 score
-    highest_f1_file = 'highest_f1_score.txt'
     if os.path.exists(highest_f1_file):
         with open(highest_f1_file, 'r') as file:
             highest_f1_score = float(file.read())
     else:
         highest_f1_score = 0.0
 
-    if mean_f1_score > highest_f1_score:
+    if best_score > highest_f1_score:
         with open(highest_f1_file, 'w') as file:
-            file.write(str(mean_f1_score))
-        highest_f1_score = mean_f1_score
-        print(f"New highest validation F1 Score: {highest_f1_score}!!!\n")
+            file.write(str(best_score))
+        highest_f1_score = best_score
+        print(f"New highest validation F1 Score: {highest_f1_score}")
     else:
-        print(f"Highest validation F1 Score remains: {highest_f1_score}\n")
+        print(f"Highest validation F1 Score remains: {highest_f1_score}")
 
     model.fit(X_train, y_train)
     return model
